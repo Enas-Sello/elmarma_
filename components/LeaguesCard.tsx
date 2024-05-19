8946f31c1d35d6c04eb517e435bd4449db14f22f
enas sello <enas.awad169@gmail.com>
2024-05-19 23:12:08 +0300

    error frix

--- components/LeaguesCard.tsx
+++ components/LeaguesCard.tsx
@@ -1,14 +1,13 @@
+import { Link } from 'lucide-react';
 import React from 'react';
 
-const LeaguesCard = (leagues: any) => {
+const LeaguesCard = ({ leagues }: any[]) => {
   console.log(leagues);
   return (
     <>
       {leagues &&
-        leagues.map((item: any) => (
-          <>
-            <div></div>
-          </>
+        leagues?.map((item: any, i: React.Key) => (
+          <Link href="" key={i}></Link>
         ))}
     </>
   );
